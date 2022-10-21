aeea0470b586aed98393f17df7b3b11254e15a50
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-10-22 00:15:29 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -109,6 +109,8 @@ export default function Home({ theme, name, quote }) {
   return (
     <div className={`h-screen overflow-hidden w-screen ${decideBackground()}`}>
       <Head>
+        <link rel="apple-touch-icon" href="/preview/favicon.png"></link>
+        <link rel="icon" href="/preview/favicon.png" type="image/x-icon" />
         <title>
           {userName
             ? `${userName} wish's you a very happy diwali.`
